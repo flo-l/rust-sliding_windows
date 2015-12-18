3de5e44597fcc726be06203e0e5c4a3c736bb03f
flo-l <lacknerflo@gmail.com>
2015-12-18 11:15:38 +0100

    update docs

---
+++ target/doc/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['sliding_windows'] = ["impl&lt;'a, I: <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='sliding_windows/struct.Adaptor.html' title='sliding_windows::Adaptor'>Adaptor</a>&lt;'a, I&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ target/doc/sliding_windows/sliding_windows/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Adaptor","See sliding_windows for more information."],["Storage","This holds the backing allocation for the `Window` of a `Adaptor`."]]});
