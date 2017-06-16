43326287fd389e0d5343304977d2e16cb5f0de89
flo-l <lacknerflo@gmail.com>
2017-06-16 18:19:25 +0200

    update docs

--- target/doc/implementors/core/iter/traits/trait.IntoIterator.js
+++ target/doc/implementors/core/iter/traits/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["sliding_windows"] = ["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for &amp;'a Window&lt;'a, T&gt;",];
+implementors["sliding_windows"] = ["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for &amp;'a <a class=\"struct\" href=\"sliding_windows/struct.Window.html\" title=\"struct sliding_windows::Window\">Window</a>&lt;'a, T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- target/doc/sliding_windows/sidebar-items.js
+++ target/doc/sliding_windows/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Adaptor","See sliding_windows for more information."],["Storage","This holds the backing allocation for the `Window` of a `Adaptor`."]],"trait":[["IterExt",""]]});
+initSidebarItems({"struct":[["Adaptor","See sliding_windows for more information."],["Storage","This holds the backing allocation for the `Window` of a `Adaptor`."],["Window","This is the `Item` type of the `Adaptor` iterator."],["WindowIter",""],["WindowIterMut",""]],"trait":[["IterExt",""]]});
