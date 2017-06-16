07aadc7a19ba68f2fec402bf7d63a7a0eafc782d
flo-l <lacknerflo@gmail.com>
2017-06-16 18:38:31 +0200

    update docs

---
+++ target/doc/implementors/core/iter/traits/trait.ExactSizeIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["sliding_windows"] = ["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"sliding_windows/struct.WindowIter.html\" title=\"struct sliding_windows::WindowIter\">WindowIter</a>&lt;'a, T&gt;","impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"sliding_windows/struct.WindowIterMut.html\" title=\"struct sliding_windows::WindowIterMut\">WindowIterMut</a>&lt;'a, T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
